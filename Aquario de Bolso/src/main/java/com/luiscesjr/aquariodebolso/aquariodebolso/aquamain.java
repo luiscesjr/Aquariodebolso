a149eb17677667fb2913bcabdc91e4ca2127fb6a
Luis <luiscesjr>
2014-05-23 00:28:32 -0300

    - New files added - Got rid of bug in infomenu activity - New icons added - 6 new activities added

--- Aquario de Bolso/src/main/java/com/luiscesjr/aquariodebolso/aquariodebolso/aquamain.java
+++ Aquario de Bolso/src/main/java/com/luiscesjr/aquariodebolso/aquariodebolso/aquamain.java
@@ -4,6 +4,8 @@ import com.luiscesjr.aquariodebolso.aquariodebolso.util.SystemUiHider;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
@@ -13,13 +15,10 @@ import android.view.View;
 import android.content.Intent;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.FragmentManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- *
- * @see SystemUiHider
- */
 public class aquamain extends Activity {
 
         public void sendMessage(View view) {
@@ -34,32 +33,19 @@ public class aquamain extends Activity {
         startActivity(i);
     }
 
-    /**
-         * Whether or not the system UI should be auto-hidden after
-         * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
-         */
+    private static boolean isXLargeTablet(Context context) {
+        return (context.getResources().getConfiguration().screenLayout
+                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
+    }
+
         private static final boolean AUTO_HIDE = true;
 
-        /**
-         * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
-         * user interaction before hiding the system UI.
-         */
         private static final int AUTO_HIDE_DELAY_MILLIS = 3000;
 
-        /**
-         * If set, will toggle the system UI visibility upon interaction. Otherwise,
-         * will show the system UI visibility upon interaction.
-         */
         private static final boolean TOGGLE_ON_CLICK = true;
 
-        /**
-         * The flags to pass to {@link SystemUiHider#getInstance}.
-         */
         private static final int HIDER_FLAGS = SystemUiHider.FLAG_HIDE_NAVIGATION;
 
-        /**
-         * The instance of the {@link SystemUiHider} for this activity.
-         */
         private SystemUiHider mSystemUiHider;
 
         @Override
@@ -70,14 +56,11 @@ public class aquamain extends Activity {
 
             final View controlsView = findViewById(R.id.fullscreen_content_controls);
             final View contentView = findViewById(R.id.fullscreen_content);
-
-            // Set up an instance of SystemUiHider to control the system UI for
-            // this activity.
             mSystemUiHider = SystemUiHider.getInstance(this, contentView, HIDER_FLAGS);
             mSystemUiHider.setup();
             mSystemUiHider
                     .setOnVisibilityChangeListener(new SystemUiHider.OnVisibilityChangeListener() {
-                        // Cached values.
+
                         int mControlsHeight;
                         int mShortAnimTime;
 
@@ -85,10 +68,6 @@ public class aquamain extends Activity {
                         @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
                         public void onVisibilityChange(boolean visible) {
                             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-                                // If the ViewPropertyAnimator API is available
-                                // (Honeycomb MR2 and later), use it to animate the
-                                // in-layout UI controls at the bottom of the
-                                // screen.
                                 if (mControlsHeight == 0) {
                                     mControlsHeight = controlsView.getHeight();
                                 }
@@ -100,20 +79,15 @@ public class aquamain extends Activity {
                                         .translationY(visible ? 0 : mControlsHeight)
                                         .setDuration(mShortAnimTime);
                             } else {
-                                // If the ViewPropertyAnimator APIs aren't
-                                // available, simply show or hide the in-layout UI
-                                // controls.
                                 controlsView.setVisibility(visible ? View.VISIBLE : View.GONE);
                             }
 
                             if (visible) {
-                                // Schedule a hide().
+
                                 delayedHide(AUTO_HIDE_DELAY_MILLIS);
                             }
                         }
                     });
-
-            // Set up the user interaction to manually show or hide the system UI.
             contentView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -125,9 +99,6 @@ public class aquamain extends Activity {
                 }
             });
 
-            // Upon interacting with UI controls, delay any scheduled hide()
-            // operations to prevent the jarring behavior of controls going away
-            // while interacting with the UI.
             findViewById(R.id.aqua_button).setOnTouchListener(mDelayHideTouchListener);
         }
 
@@ -135,18 +106,9 @@ public class aquamain extends Activity {
         protected void onPostCreate(Bundle savedInstanceState) {
             super.onPostCreate(savedInstanceState);
 
-            // Trigger the initial hide() shortly after the activity has been
-            // created, to briefly hint to the user that UI controls
-            // are available.
             delayedHide(100);
         }
 
-
-        /**
-         * Touch listener to use for in-layout UI controls to delay hiding the
-         * system UI. This is to prevent the jarring behavior of controls going away
-         * while interacting with activity UI.
-         */
         View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
             @Override
             public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -165,10 +127,6 @@ public class aquamain extends Activity {
             }
         };
 
-        /**
-         * Schedules a call to hide() in [delay] milliseconds, canceling any
-         * previously scheduled calls.
-         */
         private void delayedHide(int delayMillis) {
             mHideHandler.removeCallbacks(mHideRunnable);
             mHideHandler.postDelayed(mHideRunnable, delayMillis);
