a3c4b1ded9d89a1a8744e45ca80c263e0b55272d
Luis <luiscesjr>
2014-10-16 20:54:30 -0300

    - Added new activities - Added new functionalities such as search, tools, etc. - Other functionalities such as zoom, gifs and swipe are in the works to be added, and are already on this commit.

--- Aquario de Bolso/src/main/java/com/luiscesjr/aquariodebolso/aquariodebolso/aquamain.java
+++ Aquario de Bolso/src/main/java/com/luiscesjr/aquariodebolso/aquariodebolso/aquamain.java
@@ -1,27 +1,138 @@
 package com.luiscesjr.aquariodebolso.aquariodebolso;
 
-import com.luiscesjr.aquariodebolso.aquariodebolso.util.SystemUiHider;
-
-import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.res.Configuration;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.view.GestureDetector;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.content.Intent;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.RelativeLayout;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.luiscesjr.aquariodebolso.aquariodebolso.helper.GifDecoderView;
+
+import java.io.IOException;
+import java.io.InputStream;
+
 
 public class aquamain extends Activity {
+
+    private GestureDetector gestureDetector;private GestureDetector gesturedetector = null;
+
+    private Intent i;
+
+    private FrameLayout flContainer;
+    private LinearLayout ivLayer1 , ivLayer2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aquamain);
+
+/*
+        //Código para desenhar GIFs na tela, trabalho pela frente...
+
+        InputStream stream = null;
+        try {
+            stream = getAssets().open("betta.gif");
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        GifDecoderView view = new GifDecoderView(this, stream);
+        setContentView(view);
+
+/*
+        //Código para abrir uma nova janela usando SWIPE.
+        //ainda não faz exatamente o que quero...
+
+        flContainer = (FrameLayout) findViewById(R.id.flContainer);
+        ivLayer1 = (LinearLayout) findViewById(R.id.layTopUp);
+
+        gesturedetector = new GestureDetector(new MyGestureListener());
+
+        flContainer.setOnTouchListener(new View.OnTouchListener() {
+
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                gesturedetector.onTouchEvent(event);
+
+                return true;
+
+            }
+
+        });
+    }
+
+
+
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+
+        super.dispatchTouchEvent(ev);
+
+        return gesturedetector.onTouchEvent(ev);
+
+    }
+
+class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
+
+    private static final int SWIPE_MIN_DISTANCE = 120;
+
+    private static final int SWIPE_MAX_OFF_PATH = 100;
+
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    @Override
+    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
+
+                           float velocityY) {
+
+        float dX = e2.getX() - e1.getX();
+
+        float dY = e1.getY() - e2.getY();
+
+        if (Math.abs(dY) < SWIPE_MAX_OFF_PATH &&
+
+                Math.abs(velocityX) >= SWIPE_THRESHOLD_VELOCITY &&
+
+                Math.abs(dX) >= SWIPE_MIN_DISTANCE) {
+
+            if (dX > 0) {
+
+                Toast.makeText(getApplicationContext(), "Bem vindo!",
+                        Toast.LENGTH_SHORT).show();
+
+                Intent i = new Intent(aquamain.this, infomenu.class);
+                startActivity(i);
+
+            }
+
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+}
+
+*/
+
+    }
+
+private static final boolean ALWAYS_SIMPLE_PREFS = false;
 
     public void sendMessage(View view) {
 
@@ -35,30 +146,30 @@ public class aquamain extends Activity {
         startActivity(i);
     }
 
+    public boolean onIsMultiPane() {
+        return isXLargeTablet(this) && !isSimplePreferences(this);
+    }
+
     private static boolean isXLargeTablet(Context context) {
         return (context.getResources().getConfiguration().screenLayout
                 & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_aquamain);
+    private static boolean isSimplePreferences(Context context) {
+        return ALWAYS_SIMPLE_PREFS
+                || Build.VERSION.SDK_INT < Build.VERSION_CODES.HONEYCOMB
+                || !isXLargeTablet(context);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.infomenu, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
